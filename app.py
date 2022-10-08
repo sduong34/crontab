743e7d0153403afd7f325a3e33c7468d14b31333
sduong34 <sarahduong34@gmail.com>
2022-10-07 22:22:05 -0500

    final

---
+++ app.py
@@ -0,0 +1,24 @@
+import os 
+import sys 
+import crontab
+import time
+import pandas as pd 
+ 
+#import through API 
+df = pd.read_json('https://healthdata.gov/resource/gskt-tzcc.json')
+df.to_csv('data/covid.csv', index = None)
+
+#get and print cwd
+cwd = os.getcwd()
+print(cwd)
+
+current_time = time.time()
+print(current_time)
+
+
+start_time = time.strftime("%Y-%m-%D_%H:%M:%S", time.localtime(current_time))
+print('Program started at: ', start_time)
+
+#create a new file in the current working directory
+with open(cwd + '/home/Sarah/crontab/testFile_' + start_time + '.txt', 'w') as f:
+    f.write(str(df)) 
---
+++ instructions.md
@@ -0,0 +1,11 @@
+Every day at 8am: 0 8 * * * 
+Every Sunday at 10pm: 0 22 * * SUN
+End of every quarter: 0 0 1 */3 *
+
+Instructions: 
+1. Open VM console 
+2. Clone crontab repo 
+3. cd into crontab repo 
+4. Enter cron job commands above  
+5. systemctl status cron 
+
